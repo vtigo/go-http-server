99e95a6b24634ddb4c5ee45ab9184f43a94b37df
tigo <rf.tigas@gmail.com>
2025-04-11 13:01:54 -0300

    Read the request and fix the response

--- main.go
+++ main.go
@@ -1,39 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
+    "fmt"
+    "log"
+    "net"
 )
 
 const PORT = 3333
 
 func main() {
-	conn := startServer(PORT)
+    conn := startServer(PORT)
+    defer conn.Close()
 
-	buffer := make([]byte, 1024)
-	conn.Read(buffer)
-	request := string(buffer)
-
-	fmt.Println(request)
-
-	conn.Write([]byte("HTTP/1.1 200 OK"))
+    buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		log.Printf("Error reading the request: %v", err)
+		return
+	}
+    
+    conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
 
 func startServer(port int) net.Conn {
-	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
-	if err != nil {
-		errMessage := fmt.Sprintf("Error binding to port %d: %s", port, err )
-		log.Fatal(errMessage)
-	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		errMessage := fmt.Sprintf("Error accepting connection: %d", err )
-		log.Fatal(errMessage)
-	}
-
-	return conn
+    l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
+    if err != nil {
+        errMessage := fmt.Sprintf("Error binding to port %d: %s", port, err)
+        log.Fatal(errMessage)
+    }
+    conn, err := l.Accept()
+    if err != nil {
+        errMessage := fmt.Sprintf("Error accepting connection: %s", err)
+        log.Fatal(errMessage)
+    }
+    return conn
 }
-
-
