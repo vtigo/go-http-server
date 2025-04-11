319df8cc14ed97b86439acc93b342f16951ab7e8
tigo <rf.tigas@gmail.com>
2025-04-11 13:10:55 -0300

    Keep listening...

--- main.go
+++ main.go
@@ -9,29 +9,35 @@ import (
 const PORT = 3333
 
 func main() {
-    conn := startServer(PORT)
-    defer conn.Close()
+	l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", PORT) )
+    if err != nil {
+        errMessage := fmt.Sprintf("Error binding to port %d: %s", PORT, err)
+        log.Fatal(errMessage)
+    }
+	defer l.Close()
 
-    buffer := make([]byte, 1024)
+	log.Printf("Server listening on port %d", PORT)
+	
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %s", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
 	_, err := conn.Read(buffer)
 	if err != nil {
 		log.Printf("Error reading the request: %v", err)
 		return
 	}
-    
-    conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-}
 
-func startServer(port int) net.Conn {
-    l, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
-    if err != nil {
-        errMessage := fmt.Sprintf("Error binding to port %d: %s", port, err)
-        log.Fatal(errMessage)
-    }
-    conn, err := l.Accept()
-    if err != nil {
-        errMessage := fmt.Sprintf("Error accepting connection: %s", err)
-        log.Fatal(errMessage)
-    }
-    return conn
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	log.Printf("Handled connection from %s", conn.RemoteAddr())
 }
