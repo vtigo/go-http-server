5373a9f6d8bf405c7345223b1145f32c97413d9e
tigo <rf.tigas@gmail.com>
2025-04-12 20:51:47 -0300

    Parse request

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net"
+	"fmt"
+	"log"
+	"net"
+	"strings"
 )
 
 const PORT = 3333
@@ -32,12 +33,38 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	buffer := make([]byte, 1024)
-	_, err := conn.Read(buffer)
+	n, err := conn.Read(buffer)
 	if err != nil {
 		log.Printf("Error reading the request: %v", err)
 		return
 	}
+	requestString := string(buffer[:n])
+	requestLine, requestHeaders, requestBody := parseRequest(requestString)
+	
+	fmt.Printf("line: %s\n", requestLine)
+	fmt.Printf("headers: %v\n", requestHeaders)
+	fmt.Printf("body: %s\n", requestBody)
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	log.Printf("Handled connection from %s", conn.RemoteAddr())
+}
+
+func parseRequest(request string) (string, map[string]string, string) {
+	parts := strings.SplitN(request, "\r\n\r\n", 2)
+	headerLines := strings.Split(parts[0], "\r\n")
+	body := ""
+	if len(parts[1]) > 1 {
+		body = parts[1]
+	}
+
+	headers := make(map[string]string)
+	for i:=1; i < len(headerLines); i++ {
+		if headerLines[i] == " " {
+			continue
+		}
+		parts := strings.SplitN(headerLines[i], ": ", 2)
+		headers[parts[0]] = parts[1]
+	}
+
+	return string(headerLines[0]), headers, body
 }
